457f85c07dbfbe43963421ecadd823cac2ca76cc
Per Odlund <per.odlund@gmail.com>
2024-01-22 07:25:45 +0100

    Alomst working lua

--- src/bin/sketchover.rs
+++ src/bin/sketchover.rs
@@ -1,10 +1,9 @@
 use std::path::Path;
+use std::rc::Rc;
 
 use calloop::EventLoop;
 use mlua::{Function, IntoLuaMulti, Table, Value};
 use mlua::{Lua, UserData, UserDataMethods};
-use sketchover::mousemap::Mouse;
-// use rlua::{Table, Value};
 use sketchover::output::OutPut;
 use sketchover::runtime::Events;
 use sketchover::runtime::Runtime;
@@ -13,48 +12,54 @@ use smithay_client_toolkit::seat::keyboard::{KeyEvent, Modifiers};
 use xdg::BaseDirectories;
 
 struct LuaBindings {
-    lua: Lua,
+    lua: Rc<Lua>,
 }
-// #[derive(Clone, Debug)]
-// enum Command {
-//     Exit,
-// }
-//
-// struct RuntimeData<'a>(&'a mut Runtime<LuaBindings>);
-//
-// impl<'a> UserData for RuntimeData<'a> {
-//     fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
-//         methods.add_method("quit", |_, rt, ()| {
-//             rt.0.exit();
-//             Ok(())
-//         });
-//         methods.add_method("undo", |_, rt, ()| {
-//             rt.0.exit();
-//             Ok(())
-//         });
-//         methods.add_method("pause", |_, rt, ()| {
-//             rt.0.exit();
-//             Ok(())
-//         });
-//         methods.add_method_mut("clear", |_, rt, ()| {
-//             rt.0.clear(true);
-//             Ok(())
-//         });
-//         methods.add_method_mut("run", |_, rt, ()| {
-//             // rt.0.run();
-//             Ok(())
-//         });
-//     }
-// }
-//
-// ---@class keyevent
-// ---@field legs integer
-// ---@field eyes integer
-// sketchover.on("key", function(keyevent))
-//
-//
-//
-//
+
+impl LuaBindings {
+    fn new(lua: Rc<Lua>) -> Self {
+        LuaBindings { lua }
+    }
+}
+
+struct RuntimeData(Runtime<LuaBindings>);
+
+impl UserData for RuntimeData {
+    fn add_methods<'lua, M: UserDataMethods<'lua, Self>>(methods: &mut M) {
+        methods.add_method("quit", |_, rt, ()| {
+            rt.0.exit();
+            Ok(())
+        });
+        methods.add_method_mut("undo", |_, rt, ()| {
+            rt.0.undo();
+            Ok(())
+        });
+        methods.add_method_mut("pause", |_, rt, ()| {
+            rt.0.set_pause(true);
+            Ok(())
+        });
+        methods.add_method_mut("clear", |_, rt, ()| {
+            rt.0.clear(true);
+            Ok(())
+        });
+        methods.add_function("keypress", |lua, func: Function| {
+            register_event(lua, ("keypress".to_owned(), func))?;
+            Ok(())
+        });
+        methods.add_function("new_output", |lua, func: Function| {
+            register_event(lua, ("new_output".to_owned(), func))?;
+            Ok(())
+        });
+        methods.add_function("remove_output", |lua, func: Function| {
+            register_event(lua, ("remove_output".to_owned(), func))?;
+            Ok(())
+        });
+        methods.add_method_mut("run", |_, rt, ()| {
+            let event_loop = EventLoop::try_new().expect("couldn't create event-loop");
+            rt.0.run(event_loop);
+            Ok(())
+        });
+    }
+}
 
 struct LuaKeyEvent {
     modifiers: Modifiers,
@@ -63,39 +68,40 @@ struct LuaKeyEvent {
 
 impl UserData for LuaKeyEvent {
     fn add_fields<'lua, F: mlua::prelude::LuaUserDataFields<'lua, Self>>(fields: &mut F) {
-        fields.add_field("modifer", true)
+        fields.add_field_method_get("key", |_, event| Ok(event.key.keysym.name()));
+        fields.add_field_method_get("modifiers", |lua, event| {
+            let table = lua.create_table()?;
+            table.set("ctrl", event.modifiers.ctrl)?;
+            table.set("alt", event.modifiers.ctrl)?;
+            table.set("shift", event.modifiers.ctrl)?;
+            table.set("caps_lock", event.modifiers.ctrl)?;
+            Ok(table)
+        });
     }
 }
 
 impl Events for LuaBindings {
-    // fn init(&mut self) {
-    //     if let Ok(xdg_dirs) = BaseDirectories::with_prefix("sketchover") {
-    //         let config_dir = xdg_dirs.get_config_home();
-    //         let path = format!(
-    //             "package.path = package.path .. ';{}sketchover.lua'",
-    //             config_dir.as_os_str().to_str().unwrap()
-    //         );
-    //         self.lua.load(&path).exec().unwrap();
-    //     }
-    // }
     fn new_output(r: &mut Runtime<Self>, ouput: &mut OutPut) {
+        let lua = &r.data.lua.clone();
         let globals = r.data.lua.globals();
+
+        // emit_sync_callback(lua, ("new_output".to_owned(), args));
     }
 
     fn keybinding(r: &mut Runtime<Self>, event: KeyEvent) {
-        let lua = &r.data.lua;
+        let lua = &r.data.lua.clone();
         let modifiers = r.modifiers();
 
         let args = LuaKeyEvent {
             modifiers,
             key: event,
         };
-        emit_sync_callback(lua, ("keypress".to_owned(), args));
 
-        // struct RuntimeData<'a>(&'a mut Runtime<LuaBindings>);
+        emit_sync_callback(lua, ("keypress".to_owned(), (args))).expect("callback failed");
     }
-
-    fn mousebinding(r: &mut Runtime<Self>, button: u32) {}
+    fn mousebinding(r: &mut Runtime<Self>, button: u32) {
+        r.start_drawing(Box::new(Pen::new()));
+    }
 }
 
 pub fn emit_sync_callback<'lua, A>(
@@ -119,27 +125,6 @@ where
     }
 }
 
-pub fn get_or_create_module<'lua>(lua: &'lua Lua, name: &str) -> anyhow::Result<mlua::Table<'lua>> {
-    let globals = lua.globals();
-    let package: Table = globals.get("package")?;
-    let loaded: Table = package.get("loaded")?;
-
-    let module = loaded.get(name)?;
-    match module {
-        Value::Nil => {
-            let module = lua.create_table()?;
-            loaded.set(name, module.clone())?;
-            Ok(module)
-        }
-        Value::Table(table) => Ok(table),
-        wat => anyhow::bail!(
-            "cannot register module {} as package.loaded.{} is already set to a value of type {}",
-            name,
-            name,
-            wat.type_name()
-        ),
-    }
-}
 pub fn register_event<'lua>(
     lua: &'lua Lua,
     (name, func): (String, mlua::Function),
@@ -165,34 +150,50 @@ pub fn register_event<'lua>(
     }
 }
 
-pub fn make_lua_context(config_file: &Path) -> anyhow::Result<Lua> {
-    let lua = Lua::new();
+pub fn get_or_create_module(lua: Rc<Lua>, name: &str) -> anyhow::Result<()> {
+    let globals = lua.globals();
+    let package: Table = globals.get("package")?;
+    let loaded: Table = package.get("loaded")?;
 
-    let config_dir = config_file.parent().unwrap_or_else(|| Path::new("/"));
+    let module = loaded.get(name)?;
+    match module {
+        Value::Nil => {
+            let binds = LuaBindings::new(lua.clone());
 
-    {
-        let globals = lua.globals();
-        let sketchover = get_or_create_module(&lua, "sketchover")?;
-
-        sketchover.set("");
-        sketchover.set("on", lua.create_function(register_event)?)?;
+            let rt = Runtime::init(binds);
+            let data = RuntimeData(rt);
+            loaded.set("sketchover", data)?;
+            Ok(())
+        }
+        Value::UserData(_) => Ok(()),
+        wat => anyhow::bail!(
+            "cannot register module {} as package.loaded.{} is already set to a value of type {}",
+            name,
+            name,
+            wat.type_name()
+        ),
     }
+}
 
-    Ok(lua)
+pub fn make_lua_context(config_file: &Path) -> anyhow::Result<()> {
+    let lua = Rc::new(Lua::new());
+
+    get_or_create_module(lua.clone(), "sketchover")?;
+    let path = format!(
+        "package.path = package.path .. ';{}?.lua;'",
+        config_file.to_str().unwrap()
+    );
+    println!("path: {}", &path);
+    lua.load(&path).exec().unwrap();
+    lua.load("require('mmm')").exec().unwrap();
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
     if let Ok(xdg_dirs) = BaseDirectories::with_prefix("sketchover") {
-        let mut config = xdg_dirs.get_config_home();
-        config.push("init.lua");
-        let lua = make_lua_context(config.as_path())?;
-        let binds = LuaBindings { lua };
-
-        let mut rt = Runtime::init(binds);
-        let event_loop = EventLoop::try_new().expect("couldn't create event-loop");
-        rt.run(event_loop);
-        Ok(())
-    } else {
-        Ok(())
+        let config = xdg_dirs.get_config_home();
+        make_lua_context(config.as_path())?;
     }
+    Ok(())
 }
